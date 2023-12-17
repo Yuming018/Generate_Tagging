ddeaed482bdf79e81fa5dac90cad06ea8eaf7dc5
Yuming018 <yu4911972@gmail.com>
2023-12-18 01:02:44 +0800

    調整評估 Event 的方法，更改為類似 NER 任務的方式

--- eval_pred.py
+++ eval_pred.py
@@ -62,10 +62,10 @@ class eval_Realtion:
     def bleu_eval(self):
         record = []
         bleu_score = [0, 0, 0, 0]
+        keys_to_check = ['Relation', 'Event1', 'Event2']
         for key in tqdm(self.pred_dict):
             pred = self.pred_dict[key]
             tar = self.tar_dict[key]
-            keys_to_check = ['Relation', 'Event1', 'Event2']
             if not all(key in pred for key in keys_to_check) or not all(key in tar for key in keys_to_check):
                 continue
             pred_label, true_label = 0, 0
@@ -118,32 +118,67 @@ class eval_Realtion:
         return record, score
     
 class eval_Event:
-    def __init__(self) -> None:
-        pass
-    
-    def bleu_eval(self, dataset):
-        record = []
-        bleu_score = [0, 0, 0, 0]
-        for data in tqdm(dataset):
-            pred, tar =  data[2], data[3]
-            result = metric.compute(predictions=[pred], references=[tar])
-            bleu_1 = round(result['precisions'][0], 2)
-            bleu_2 = round(result['precisions'][1], 2)
-            bleu_3 = round(result['precisions'][2], 2)
-            bleu_4 = round(result['precisions'][3], 2)
-            data = np.concatenate((data, [bleu_1, bleu_2, bleu_3, bleu_4]))
-            bleu_score[0] += bleu_1
-            bleu_score[1] += bleu_2
-            bleu_score[2] += bleu_3
-            bleu_score[3] += bleu_4
-            record.append(data)
-        return record, bleu_score
+    def __init__(self, path) -> None:
+        self.pred_dict, self.tar_dict = defaultdict(defaultdict), defaultdict(defaultdict)
+        self.all_type_dict = dict()
+        self.dataset = read_data(path)
+        self.process_data()
 
-    def SentenceTransformer_eval(self, dataset):
+    def process_data(self):
+        type_idx = 0
+        for idx, data in tqdm(enumerate(self.dataset)):
+            pred, tar =  data[2], data[3]
+            pred = pred.split('[')[1:-1]
+            tar = tar.split('[')[1:-1]
+            self.pred_dict[idx]['data'] = data
+            for p in pred:
+                if p.split(']')[0] != 'Event' and p.split(']')[0] not in self.all_type_dict:
+                    self.all_type_dict[p.split(']')[0]] = type_idx
+                    type_idx += 1
+                self.pred_dict[idx][p.split(']')[0]] = p.split(']')[1]
+            for t in tar:
+                if t.split(']')[0] != 'Event' and t.split(']')[0] not in self.all_type_dict:
+                    self.all_type_dict[t.split(']')[0]] = type_idx
+                    type_idx += 1
+                self.tar_dict[idx][t.split(']')[0]] = t.split(']')[1]
+        self.all_type_dict['other'] = type_idx
+
+    def NER_eval(self):
+        pred, true = [], []
+        keys_to_check = ['data', 'Event']
+        for key in tqdm(self.pred_dict):
+            predict = self.pred_dict[key]
+            tar = self.tar_dict[key]
+            for key, entity in tar.items():
+                if key not in keys_to_check:
+                    bleu_score, max_type = 0, self.all_type_dict['other']
+                    entity_len = len(entity.split())
+                    for p_key, p_entity in predict.items():
+                        if p_key not in keys_to_check:
+                            result = metric.compute(predictions=[entity], references=[p_entity])
+                            if result['precisions'][min(4, entity_len)-1] > bleu_score:
+                                bleu_score = result['precisions'][min(4, entity_len)-1]
+                                max_type = p_key
+                    if bleu_score > 0.8:
+                        pred.append(self.all_type_dict[max_type])
+                    else:
+                        pred.append(self.all_type_dict['other'])
+                    true.append(self.all_type_dict[key])
+        
+        precision = precision_score(true, pred, average='micro')
+        recall = recall_score(true, pred, average='micro')
+        f1_s = f1_score(true, pred, average='micro')
+        print(f'Micro-average Precision: {precision}')
+        print(f'Micro-average Recall: {recall}')
+        print(f'Micro-average F1_score: {f1_s}')
+        print(classification_report(true, pred))
+        # print(confusion_matrix(true, pred))
+
+    def SentenceTransformer_eval(self):
         record = []
         score = 0
         model = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
-        for data in tqdm(dataset):
+        for data in tqdm(self.dataset):
             pred, tar =  data[2], data[3]
             query_embedding = model.encode(pred)
             passage_embedding = model.encode(tar)
@@ -163,20 +198,20 @@ if __name__ == '__main__':
     print('Generation : ', args.Generation, '\n')
 
     path = f'save_model/{args.Type}/{args.Generation}/'
-    # data = read_data(path + f'{args.Generation}.csv')
-
     if args.Type == 'Event':
         eval = eval_Event(path + f'{args.Generation}.csv')
+        eval.NER_eval()
+        record, s_score = eval.SentenceTransformer_eval()
     elif args.Type == 'Relation':
         eval = eval_Realtion(path + f'{args.Generation}.csv')
         eval.process_label()
+        record, bleu_score = eval.bleu_eval()
+        record, s_score = eval.SentenceTransformer_eval(record)
+        num = len(record)
+        print("BLEU-1 : ", round(bleu_score[0]/num, 2))
+        print("BLEU-2 : ", round(bleu_score[1]/num, 2))
+        print("BLEU-3 : ", round(bleu_score[2]/num, 2))
+        print("BLEU-4 : ", round(bleu_score[3]/num, 2))
 
-    record, bleu_score = eval.bleu_eval()
-    record, s_score = eval.SentenceTransformer_eval(record)
-    num = len(record)
-    print("BLEU-1 : ", round(bleu_score[0]/num, 2))
-    print("BLEU-2 : ", round(bleu_score[1]/num, 2))
-    print("BLEU-3 : ", round(bleu_score[2]/num, 2))
-    print("BLEU-4 : ", round(bleu_score[3]/num, 2))
-    print("SentenceTransformer : ", round(s_score/num, 2))
+    print("SentenceTransformer : ", round(s_score/len(record), 2))
     save_csv(record, path + 'score.csv')
